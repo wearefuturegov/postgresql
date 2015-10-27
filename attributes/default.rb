9741933c545bb2db4c86b28977db1c31268bd1cc
Andrew France <andrew@avito.co.uk>
2015-10-27 15:45:35 +0000

    Temporarily fix deploys on CentOS 7.
    Looks like the PGDG package installs itself as a new repo, an upgrade has
    been released but it continues to try and install the old package.

--- attributes/default.rb
+++ attributes/default.rb
@@ -245,7 +245,7 @@ default['postgresql']['pgdg']['repo_rpm_url'] = {
     },
     "centos" => {
       "7" => {
-        "x86_64" => "http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm"
+        "x86_64" => "http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-2.noarch.rpm"
       },
       "6" => {
         "i386" => "http://yum.postgresql.org/9.3/redhat/rhel-6-i386/pgdg-centos93-9.3-1.noarch.rpm",
